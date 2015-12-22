b16579ecc9e11638b6a8ba7c5604d23c95d5321b
ZHOUJiemin <jieminzhou1987@gmail.com>
2015-12-22 14:16:53 +0800

    Commit@20151222
    Started a new project
    Built test bench top level

---
+++ tb/tb_top.sv
@@ -0,0 +1,20 @@
+//Testbench Top
+//Description: UVM practice on DUT 01
+//Modification History
+//Date          Author          Description
+//2015.12.22    ZHOU Jiemin     First created
+
+//Source Code Starts Here------------------------------------
+module tb_top();
+
+  //interface instantiation
+
+  //dut instantiation
+
+  //test instantiation
+
+  //config database
+
+  //run test
+  
+endmodule
